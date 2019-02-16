8a8e35c79a2165836002a06829174bb7ff0fb25c
Dyego Maas <dyego.maas@gmail.com>
2019-02-16 15:32:10 -0200

    Encapsulando informação retornada.

---
+++ tests/test_forecasts.py
@@ -0,0 +1,40 @@
+from web_apis.forecasts import ForecastData
+import pytest
+import json
+
+
+class TestForecastDataExtraction:
+
+    @pytest.fixture()
+    def valid_forecast_data(self):
+        with open('valid_forecast.json') as file:
+            file_content = file.read()
+            return json.loads(file_content)
+
+    @pytest.fixture()
+    def invalid_forecast_data(self):
+        return json.loads('{"cod":"404","message":"city not found"}')
+
+    def test_forecast_was_found(self, valid_forecast_data):
+        forecast = ForecastData(valid_forecast_data)
+
+        assert forecast.data_was_found() is True
+
+    def test_forecast_was_not_found_for_some_city(self, invalid_forecast_data):
+        forecast = ForecastData(invalid_forecast_data)
+
+        assert forecast.data_was_found() is False
+
+    def test_extracts_nothing_from_an_invalid_response(self, invalid_forecast_data):
+        forecast_data = ForecastData(invalid_forecast_data)
+
+        forecast_list = forecast_data.extract()
+
+        assert len(forecast_list) == 0
+
+    def test_extracts_list_from_a_valid_response(self, valid_forecast_data):
+        forecast_data = ForecastData(valid_forecast_data)
+
+        forecast_list = forecast_data.extract()
+
+        assert len(forecast_list) == 40
--- web_apis/forecasts.py
+++ web_apis/forecasts.py
@@ -1,4 +1,29 @@
+import requests
+
+
 class OpenWeatherAPI:
 
     def verify_city_exists(self, city_name) -> bool:
-        pass
+        forecast = self.get_city_forecast_for_next_5_days(city_name)
+        return forecast['cod'] == 200
+
+    def get_city_forecast_for_next_5_days(self, city_name):
+        """Atualmente suporta apenas cidades brasileiras."""
+
+        appid = 'eb8b1a9405e659b2ffc78f0a520b1a46'  # TODO move to an environment variable
+        url = f' http://api.openweathermap.org/data/2.5/forecast?q={city_name},br&mode=json&appid={appid}'
+        forecast = requests.get(url)
+
+        return forecast.json()
+
+
+class ForecastData:
+
+    def __init__(self, forecast_data):
+        self.data = forecast_data
+
+    def data_was_found(self) -> bool:
+        return self.data["cod"] == '200'
+
+    def extract(self) -> dict:
+        return self.data["list"] if self.data_was_found() else []
