037c6b2a89bc5b5cfde67c8ab854be5f65e5a964
Dyego Alekssander Maas <dyego.maas@hbsis.com.br>
2019-02-20 17:54:37 -0300

    Alteradas unidades de medida para sistema métrico.

--- web_apis/forecasts.py
+++ web_apis/forecasts.py
@@ -11,7 +11,7 @@ class OpenWeatherMapAPI:
     def get_city_forecast_for_next_5_days(self, city_name):
         """Currently, it supports only brazilian cities."""
 
-        query_string = urllib.parse.urlencode({'q': city_name, 'mode': 'json', 'appid': self.app_id})
+        query_string = urllib.parse.urlencode({'q': city_name, 'units': 'metric', 'mode': 'json', 'appid': self.app_id})
         url = f' http://api.openweathermap.org/data/2.5/forecast?{query_string}'
         json = requests.get(url).json()
 
